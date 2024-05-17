41176290c1a6b439e653cfa0d9bc5622c42b123a
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-05-17 15:41:48 -0400

    too much not adding up

--- FE-CreeperKeeper/src/App.jsx
+++ FE-CreeperKeeper/src/App.jsx
@@ -1,43 +1,24 @@
-import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import UserRouter from "./routes.jsx";
 import "./App.css";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  if (isLoggedIn) {
-    return (
-      <div className="server-menu">
-        <div>
-          <form>
-            <label htmlFor="server-dropdown">Select Version</label>
-            <select id="server-dropdown"></select>
-            <button>Create Server</button>
-          </form>
-        </div>
-
-        <div>
-          <form>
-            <label htmlFor="server-dropdown">Select Saved Instance</label>
-            <select id="server-dropdown"></select>
-            <button>Startup Server</button>
-          </form>
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <div className="login-form">
-      <h1>Please login to begin</h1>
-      <form>
-        <label htmlFor="username">Username</label>
-        <input id="username"></input>
-        <label htmlFor="password">Password</label>
-        <input id="password"></input>
-      </form>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <>
+              <Authenticator loginMechanism="email" />
+              <UserRouter />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
