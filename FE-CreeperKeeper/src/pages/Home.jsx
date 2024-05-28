1c7c8cd779ddc786d50c11dea5885ab9c831f0ed
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-05-28 12:55:23 -0400

    home.jsx

--- FE-CreeperKeeper/src/pages/Home.jsx
+++ FE-CreeperKeeper/src/pages/Home.jsx
@@ -1,33 +1,3 @@
-//import React, { useState } from "react";
-//
-//export default function LandingPage() {
-//  const [isAuthorized, setIsAuthorized] = useState(true);
-//
-//  if (isAuthorized) {
-//    return (
-//      <div className="server-menu">
-//        <div>
-//          <form>
-//            <label htmlFor="server-dropdown">Select Version</label>
-//            <select id="server-dropdown"></select>
-//            <button>Create Server</button>
-//          </form>
-//        </div>
-//
-//        <div>
-//          <form>
-//            <label htmlFor="server-dropdown">Select Saved Instance</label>
-//            <select id="server-dropdown"></select>
-//            <button>Startup Server</button>
-//          </form>
-//        </div>
-//      </div>
-//    );
-//  }
-//
-//  return <Authenticator loginMechanism={["email"]} />;
-//}
-
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
@@ -38,10 +8,27 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <h1>Hello World</h1>
-      <p>See console log for Amazon Cognito user tokens.</p>
-      <button onClick={handleLogout}>Logout</button>
+    <div className="server-menu">
+      <div>
+        <form>
+          <label htmlFor="server-dropdown">Select Version</label>
+          <select id="server-dropdown"></select>
+          <button>Create Server</button>
+        </form>
+      </div>
+
+      <div>
+        <form>
+          <label htmlFor="server-dropdown">Select Saved Instance</label>
+          <select id="server-dropdown"></select>
+          <button>Startup Server</button>
+        </form>
+      </div>
+      <div>
+        <h1>Hello World</h1>
+        <p>See console log for Amazon Cognito user tokens.</p>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
     </div>
   );
 };
