fb2ce7a0076369b6251b772a4a620bda58906855
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-11-09 06:42:16 -0500

    saving

--- BE-CreeperKeeper/functions/creeper-keeper-connect-websocket/main.go
+++ BE-CreeperKeeper/functions/creeper-keeper-connect-websocket/main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, event events.APIGatewayWebsocketProxyRequest) error {
+func handler(ctx context.Context, event events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Printf("Received message %+v", event)
 
-	return nil
+	log.Printf("Connection established: %s", event.RequestContext.ConnectionID)
+	// Add your connection handling logic here
+	return events.APIGatewayProxyResponse{StatusCode: http.StatusOK}, nil
 }
 
 func main() {
