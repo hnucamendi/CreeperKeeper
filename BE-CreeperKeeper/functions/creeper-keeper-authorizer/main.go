3494bae6d1bf4211f3fa3940c6126372fb9e41ea
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-11-09 22:06:56 -0500

    fix authorizer

--- BE-CreeperKeeper/functions/creeper-keeper-authorizer/main.go
+++ BE-CreeperKeeper/functions/creeper-keeper-authorizer/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"context"
 	"log"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/hnucamendi/jwt-go/jwt"
+	"github.com/hnucamendi/CreeperKeeper/BE-CreeperKeeper/functions/creeper-keeper-authorizer/vendor/github.com/hnucamendi/jwt-go/jwt"
 )
+
+var (
+	j *jwt.JWTClient
+)
+
+func init() {
+	j = jwt.NewJWTClient(
+		jwt.JWTTenantURL("https://dev-bxn245l6be2yzhil.us.auth0.com"),
+	)
+}
 
 func generateAllowPolicy(arn string) events.APIGatewayCustomAuthorizerResponse {
 	return events.APIGatewayCustomAuthorizerResponse{
@@ -42,13 +53,12 @@ func generateDenyPolicy(arn string) events.APIGatewayCustomAuthorizerResponse {
 }
 
 func handler(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
-	j := jwt.NewJWTClient()
-
+	token := strings.TrimSpace(strings.TrimPrefix(event.AuthorizationToken, "Bearer"))
 	log.Printf("Received message %+v", event)
 
-	err := j.ValidateToken(event.AuthorizationToken)
+	err := j.ValidateToken(token)
 	if err != nil {
-		return generateAllowPolicy(event.MethodArn), nil
+		return generateDenyPolicy(event.MethodArn), nil
 	}
 
 	return generateAllowPolicy(event.MethodArn), nil
