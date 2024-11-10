fde2c7cad577d352e3c98613e02688548aa77d97
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-11-10 16:46:51 -0500

    authorizer

--- BE-CreeperKeeper/functions/creeper-keeper-authorizer/main.go
+++ BE-CreeperKeeper/functions/creeper-keeper-authorizer/main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/hnucamendi/jwt-go/jwt"
 )
 
-var j *jwt.JWTClient
+var (
+	j  *jwt.JWTClient
+	sc *ssm.Client
+)
 
 func init() {
 	j = jwt.NewJWTClient(
 		jwt.JWTTenantURL("https://dev-bxn245l6be2yzhil.us.auth0.com"),
 	)
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		panic("unable to load SDK config, " + err.Error())
+	}
+
+	ssm = ssm.NewFromConfig(cfg)
 }
 
 func generatePolicy(principalID, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
@@ -27,7 +40,7 @@ func generatePolicy(principalID, effect, resource string) events.APIGatewayCusto
 				{
 					Action:   []string{"execute-api:Invoke"},
 					Effect:   effect,
-					Resource: []string{"*"},
+					Resource: []string{resource},
 				},
 			},
 		},
@@ -35,17 +48,49 @@ func generatePolicy(principalID, effect, resource string) events.APIGatewayCusto
 	return policy
 }
 
+func getParams(paths ...string) (map[string]string, error) {
+	params := map[string]string{}
+	for _, path := range paths {
+		param, err := sc.GetParameter(context.TODO(), &ssm.GetParameterInput{
+			Name:           &path,
+			WithDecryption: true,
+		})
+		if err != nil {
+			log.Fatalf("failed to get parameter %s: %v", path, err)
+		}
+		params[path] = param.Parameter.Value
+	}
+	return params, nil
+}
+
 func handler(ctx context.Context, event events.APIGatewayWebsocketProxyRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
 	token := strings.TrimSpace(strings.TrimPrefix(event.Headers["Authorization"], "Bearer "))
 	log.Printf("Received message %+v", event)
 
-	err := j.ValidateToken(token)
+	p, err := getParams("/accountID")
 	if err != nil {
-		log.Printf("Token validation failed: %v", err)
-		return generatePolicy("user", "Deny", event.RequestContext.ResourcePath), nil
+		log.Fatalf("failed to get parameters: %v", err)
 	}
 
-	return generatePolicy("user", "Allow", event.RequestContext.ResourcePath), nil
+	// Validate the JWT token here (assuming you have a function for that)
+	err = j.ValidateToken(token)
+	if err != nil {
+		log.Printf("Token validation failed: %v", err)
+		return generatePolicy("user", "Deny", "*"), nil
+	}
+
+	// Construct the resource ARN for the WebSocket API
+	apiID := event.RequestContext.APIID
+	stage := event.RequestContext.Stage
+	region := "us-east-1"
+	accountID := p["/accountID"]
+
+	// resourceArn := "arn:aws:execute-api:" + region + ":" + accountID + ":" + apiID + "/" + stage + "/POST/*"
+	resourceArn := fmt.Sprintf("arn:aws:execute-api:%s:%s:%s/%s/POST/%s",
+		region, accountID, apiID, stage, event.RequestContext.RouteKey)
+
+	log.Printf("Token validated successfully, generating allow policy")
+	return generatePolicy("user", "Allow", resourceArn), nil
 }
 
 func main() {
