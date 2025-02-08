817f6c9a9b5f1ce7297eabd83587ec96cc1d5f3d
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2025-02-08 17:04:18 -0500

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
 locals {
   cp_app_name = "creeperkeeper"
 }
-// sudo docker run -d --name mc -p 25565:25565 -e EULA=TRUE -e TYPE=FTBA -e FTB_MODPACK_ID=126 -e FTB_MODPACK_VERSION=100011 -e MEMORY=2G -e MAX_PLAYERS=10 -e MOTD="RedCraft" -e TZ=EST -e DIFFICULTY=3 -e OPS="Oldjimmy_" -v "$(pwd)/data:/data" --tty --interactive itzg/minecraft-server
+
 ## EC2 Server ##
 resource "aws_instance" "main" {
   ami                    = var.ami
