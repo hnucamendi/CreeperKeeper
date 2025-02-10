4e111ec94b2016bbfe68c8fa578631c58a7307b3
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2025-02-10 16:49:44 -0500

    saving

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,26 +2,15 @@ locals {
   cp_app_name = "creeperkeeper"
 }
 
-
-
-module "ftb_server" {
+module "vanilla" {
   source  = "hnucamendi/minecraft-server-module/aws"
   version = "0.0.7"
 
-  vpc_id                              = var.vpc_id
-  app_name                            = local.cp_app_name
-  instance_type                       = "t3.large"
-  minecraft_max_players               = 10
-  minecraft_motd                      = "RedCraft"
-  minecraft_ops_list                  = "Oldjimmy_"
-  minecraft_server_type               = "FTBA"
-  minecraft_rcon_cmds_last_disconnect = "stop"
-  minecraft_memory_G                  = 6
-  minecraft_difficulty_level          = 3
-  minecraft_world_name                = "RedCraft"
-  minecraft_world_seed                = local.cp_app_name
-  ftb_modpack_version_id              = 100011
-  ftb_modpack_id                      = 126
+  vpc_id             = var.vpc_id
+  app_name           = local.cp_app_name
+  instance_type      = "t3.medium"
+  minecraft_ops_list = "Oldjimmy_"
+  minecraft_memory_G = 3
 
   security_group_ingress_rules = {
     "allow-all-mc" = {
@@ -40,3 +29,41 @@ module "ftb_server" {
     }
   }
 }
+
+// Direwolf modpack
+// module "ftb_server" {
+//   source  = "hnucamendi/minecraft-server-module/aws"
+//   version = "0.0.7"
+// 
+//   vpc_id                              = var.vpc_id
+//   app_name                            = local.cp_app_name
+//   instance_type                       = "r6i.large"
+//   minecraft_max_players               = 10
+//   minecraft_motd                      = "RedCraft"
+//   minecraft_ops_list                  = "Oldjimmy_"
+//   minecraft_server_type               = "FTBA"
+//   minecraft_rcon_cmds_last_disconnect = "stop"
+//   minecraft_memory_G                  = 20
+//   minecraft_difficulty_level          = 3
+//   minecraft_world_name                = "RedCraft"
+//   minecraft_world_seed                = local.cp_app_name
+//   ftb_modpack_version_id              = 100011
+//   ftb_modpack_id                      = 126
+// 
+//   security_group_ingress_rules = {
+//     "allow-all-mc" = {
+//       description = "Allow Minecraft TCP"
+//       from_port   = 25565
+//       to_port     = 25565
+//       protocol    = "tcp"
+//       cidr_blocks = ["0.0.0.0/0"]
+//     },
+//     "allow-host-ssh" = {
+//       description = "Allow SSH"
+//       from_port   = 22
+//       to_port     = 22
+//       protocol    = "tcp"
+//       cidr_blocks = ["${var.home_ip}/32"] # Restrict to your IP address
+//     }
+//   }
+// }
