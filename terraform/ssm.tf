1b913aa34f138b789ad7a299bf32412e50599091
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2025-02-16 16:40:19 -0500

    adding ssm and fixing small consistencies

---
+++ terraform/ssm.tf
@@ -0,0 +1,29 @@
+resource "aws_ssm_parameter" "statemanager_jwt_audience" {
+  name  = "/${var.ck_app_name}/jwt/client/audience"
+  type  = "SecureString"
+  value = "changeme"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "statemanager_jwt_client_secret" {
+  name  = "/${var.ck_app_name}/jwt/client/secret"
+  type  = "SecureString"
+  value = "changeme"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "aws_ssm_parameter" "statemanager_jwt_client_id" {
+  name  = "/${var.ck_app_name}/jwt/client/id"
+  type  = "SecureString"
+  value = "changeme"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
