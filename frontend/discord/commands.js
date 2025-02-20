44a8961a52dba1aac4df213178270fffe14ab2cb
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2025-02-19 19:46:33 -0500

    discord

--- frontend/discord/commands.js
+++ frontend/discord/commands.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { listAllServers } from "./game.js";
 import { capitalize, InstallGlobalCommands } from "./utils.js";
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 // Get the game choices from game.js
 async function createCommandChoices() {
@@ -30,6 +31,23 @@ const LIST_SERVERS_COMMAND = {
   ],
 };
 
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("login")
+    .setDescription("Get a login link to authenticate with CreeperKeeper."),
+  async execute(interaction) {
+    const auth0Domain = "YOUR_AUTH0_DOMAIN";
+    const clientId = "YOUR_AUTH0_CLIENT_ID";
+    const redirectUri = encodeURIComponent(
+      "https://your-backend.com/auth/callback",
+    );
+
+    const loginUrl = `https://${auth0Domain}/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=openid profile email`;
+
+    await interaction.reply(`Click here to log in: [Login](${loginUrl})`);
+  },
+};
 const ALL_COMMANDS = [LIST_SERVERS_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
