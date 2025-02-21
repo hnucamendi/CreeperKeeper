464213a7256933422334e87d4da1969960866be7
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2025-02-21 05:19:46 -0500

    saving FE logic

--- frontend/web/src/pages/Home.tsx
+++ frontend/web/src/pages/Home.tsx
@@ -1,87 +1,171 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Logout from "../components/Logout";
 import Login from "../components/Login";
+import { HTMLFormMethod } from "react-router-dom";
 
 interface Server {
-  ID: string;
-  SK: string;
-  IP: string;
-  Name: string;
-  LastUpdated: string;
-  IsRunning: boolean;
+  serverID: string;
+  row: string;
+  serverIP: string;
+  serverName: string;
+  lastUpdated: string;
+  isRunning: boolean;
 }
 
 export default function Home(): React.ReactNode {
   const { isAuthenticated, getAccessTokenSilently } = useAuth0();
   const baseURL = "https://api.creeperkeeper.com";
 
-  const [server, setServer] = useState<Server | null>(null);
+  const [server, setServer] = useState<Array<Server> | null>(null);
   const [token, setToken] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
+  const [serverStateChange, setServerStateChange] = useState<number>(0);
+  const THREE_MINUTES: number = 60 * 3000;
+  const ONE_MINUTE: number = 60 * 1000;
 
   useEffect(() => {
-    const getAuthToken = async () => {
-      try {
-        const accessToken = await getAccessTokenSilently();
-        setToken(`Bearer ${accessToken}`);
-      } catch (error) {
-        console.error("Error getting access token:", error);
-      }
+    const gat = async () => {
+      return await getAuthToken();
     };
-
-    if (isAuthenticated) getAuthToken();
+    if (isAuthenticated) gat();
   }, [isAuthenticated, getAccessTokenSilently]);
 
   useEffect(() => {
-    const listServers = async () => {
-      const path = "/server/list";
-      const url = baseURL + path;
-
-      const body = {
-        method: "GET",
-        header: {
-          "Content-Type": "application/json",
-          Authorization: token || "",
-        },
-      };
-
-      try {
-        setLoading(true);
-        const res = await fetch(url, body);
-        if (!res.ok)
-          throw new Error(`Error fetching list of servers ${res.status}`);
-        const resJson: Server = await res.json();
-        setServer(resJson);
-      } catch (error: unknown) {
-        setLoading(false);
-        console.error((error as Error).message);
-      } finally {
-        setLoading(false);
-      }
+    const ls = async () => {
+      return await listServers();
     };
-    if (token) listServers();
-  }, [token]);
+    if (token) ls();
+  }, [token, serverStateChange]);
+
+  const getAuthToken = async () => {
+    try {
+      const accessToken = await getAccessTokenSilently();
+      setToken(`Bearer ${accessToken}`);
+    } catch (error) {
+      console.error("Error getting access token:", error);
+    }
+  };
+
+  const sleep = async (time: number): Promise<void> =>
+    new Promise((resolve) => setTimeout(resolve, time));
+
+  const listServers = async () => {
+    setLoading(true);
+    const url = new URL(baseURL + "/server/list");
+    const req = await buildRequest(url, "GET");
+    try {
+      const res = await fetch(req);
+      if (!res.ok)
+        throw new Error(`Error fetching list of servers ${res.status}`);
+      const resJson: Array<Server> = await res.json();
+      setServer(resJson);
+    } catch (error: unknown) {
+      console.error((error as Error).message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const startServer = async (serverID: string) => {
+    const url = new URL(baseURL + "/server/start");
+    const req = await buildRequest(
+      url,
+      "POST",
+      JSON.stringify({
+        serverID: serverID,
+      }),
+    );
+    try {
+      await fetch(req);
+      await sleep(THREE_MINUTES);
+    } catch (error) {
+      throw new Error(`Failed to start server ${serverID} Error: ${error} `);
+    } finally {
+      setServerStateChange((prev) => prev + 1);
+    }
+  };
+
+  const stopServer = async (serverID: string) => {
+    const url = new URL(baseURL + "/server/stop");
+    const req = await buildRequest(
+      url,
+      "POST",
+      JSON.stringify({
+        serverID: serverID,
+      }),
+    );
+
+    try {
+      await fetch(req);
+      await sleep(ONE_MINUTE);
+    } catch (error) {
+      throw new Error(`Failed to stop server ${serverID} Error: ${error} `);
+    } finally {
+      setServerStateChange((prev) => prev + 1);
+    }
+  };
+
+  const buildRequest = async (
+    url: URL,
+    method: HTMLFormMethod,
+    body?: BodyInit,
+  ): Promise<Request> => {
+    if (!token) {
+      try {
+        await getAuthToken();
+      } catch (error) {
+        throw new Error(
+          `Failed to get AuthToken when building request ${error}`,
+        );
+      }
+    }
+
+    if (!token) {
+      throw new Error(
+        "AuthToken is still missing after attempting to fetch new token",
+      );
+    }
+
+    return new Request(url, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+      ...(body != null && method !== "GET" ? { body } : {}),
+    });
+  };
 
   if (!isAuthenticated) {
     <Login />;
     return <h1>✋ Please log in ✋</h1>;
   }
 
-  if (loading) {
+  if (loading || server === null) {
     return (
       <main>
         <h1>Available servers are loading... 🐌</h1>
       </main>
     );
   }
-
-  console.log(server);
   return (
     <main>
       <Logout />
       <h1> 🌚 Welcome 🎃</h1>
+      <button onClick={listServers}>trigger list servers</button>
+      {server.map((v: Server, i: number) => (
+        <div key={i}>
+          <span>{v.row}</span>
+          <span>Last updated: {v.lastUpdated}</span>
+          <h2>{v.serverName}</h2>
+          <p>{v.serverID}</p>
+          <p>{v.isRunning ? v.serverIP : `Last server IP: ${v.serverIP}`}</p>
+          <p>Status: {v.isRunning ? "RUNNING" : "STOPPED"}</p>
+          <button onClick={() => startServer(v.serverID)}>Start</button>
+          <button onClick={() => stopServer(v.serverID)}>Stop</button>
+        </div>
+      ))}
     </main>
   );
 }
