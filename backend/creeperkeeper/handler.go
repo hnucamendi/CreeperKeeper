d10964b16e1f1fd3d5a673cb280b6ef96ec7e5b6
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2025-02-21 21:02:50 -0500

    test

--- backend/creeperkeeper/handler.go
+++ backend/creeperkeeper/handler.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
